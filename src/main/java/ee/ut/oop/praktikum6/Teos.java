06080e223c9b0899d34c8a78a8edf6cc85e9b29d
Konstantin Tenman <konstantin.tenman@cyber.ee>
2025-03-20 11:22:05 +0200

    Add solution of 4.6

--- src/main/java/ee/ut/oop/praktikum6/Teos.java
+++ src/main/java/ee/ut/oop/praktikum6/Teos.java
@@ -1,6 +1,6 @@
 package ee.ut.oop.praktikum6;
 
-public abstract class Teos {
+public abstract class Teos implements Comparable<Teos>{
     private String teoseKirjeldus;
     private String teoseTähis;
     private String laenutajaNimi;
@@ -27,5 +27,53 @@ public abstract class Teos {
             case "sinine" -> 60;
             default -> 0;
         };
+    }
+
+    public double päevaneViivis() {
+        return switch (teoseTähis) {
+            case "roheline" -> 2;
+            case "puudub" -> 0.15;
+            case "kollane", "sinine" -> 0.05;
+            default -> 0;
+        };
+    }
+
+    public void arvutaViivis(Kontrollija kontrollija) {
+        if (päevadeArv > laenutusAeg()) {
+            double viivis = (päevadeArv - laenutusAeg()) * päevaneViivis();
+            kontrollija.salvestaViivis(laenutajaNimi, teoseKirjeldus, viivis);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Teos{" +
+                "teoseKirjeldus='" + teoseKirjeldus + '\'' +
+                ", teoseTähis='" + teoseTähis + '\'' +
+                ", laenutajaNimi='" + laenutajaNimi + '\'' +
+                ", päevadeArv=" + päevadeArv + '\'' +
+                ", kas tellitud hoidlast? " + (kasHoidlast() ? "jah" : "ei") +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Teos teineTeos) {
+        return this.teoseKirjeldus.compareTo(teineTeos.teoseKirjeldus);
+    }
+
+    public String getTeoseKirjeldus() {
+        return teoseKirjeldus;
+    }
+
+    public String getTeoseTähis() {
+        return teoseTähis;
+    }
+
+    public String getLaenutajaNimi() {
+        return laenutajaNimi;
+    }
+
+    public int getPäevadeArv() {
+        return päevadeArv;
     }
 }
