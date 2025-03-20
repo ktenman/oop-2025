a097c52d3fe60a341f42e4ef005108e7e5eea83b
Konstantin Tenman <konstantin.tenman@cyber.ee>
2025-03-20 11:25:33 +0200

    Add solution of 5.2

--- src/main/java/ee/ut/oop/praktikum6/Raamat.java
+++ src/main/java/ee/ut/oop/praktikum6/Raamat.java
@@ -1,4 +1,18 @@
 package ee.ut.oop.praktikum6;
 
-public class Raamat {
+public class Raamat extends Teos{
+    public Raamat(String teoseKirjeldus, String teoseTähis, String laenutajaNimi, int päevadeArv) {
+        super(teoseKirjeldus, teoseTähis, laenutajaNimi, päevadeArv);
+    }
+
+    @Override
+    boolean kasHoidlast() {
+        return "kollane".equals(getTeoseTähis()) ||
+                "sinine".equals(getTeoseTähis());
+    }
+
+    @Override
+    public String toString() {
+        return "Raamat: " + super.toString();
+    }
 }
